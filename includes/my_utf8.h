bbfb90147d35dd2dcbcb4862945058529aca11b9
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-09-14 10:00:28 +0200

    i

--- includes/my_utf8.h
+++ includes/my_utf8.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   my_utf8.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmauvari <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/14 09:59:40 by nmauvari          #+#    #+#             */
+/*   Updated: 2018/09/14 10:00:12 by nmauvari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MY_UTF8_H
 # define  MY_UTF8_H
 
@@ -6,17 +18,17 @@
 */
 
 int				utf8_seq(
-	wchar_t val,
-	int char_lim,
-	char *put);
+	wchar_t	val,
+	int		char_lim,
+	char	*put);
 
 size_t			utf8_strtoseq(
-	wchar_t *str,
-	char *put,
-	size_t len)
+	wchar_t	*str,
+	char	*put,
+	size_t	len)
 
 int				utf8_trueseq(
-	wchar_t val,
-	char *put);
+	wchar_t	val,
+	char	*put);
 
 #endif
